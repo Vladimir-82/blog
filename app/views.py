a7ea1871a1412cf6dc714c13557cb24747ac1608
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-12-12 16:42:16 +0300

    bootstrap added

--- app/views.py
+++ app/views.py
@@ -4,4 +4,13 @@ from .models import Post, Category
 
 class MainListView(ListView):
     template_name = 'main.html'
-    model = Category
+    context_object_name = 'post'
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['post'] = Post.objects.all()
+        return context
