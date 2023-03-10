b201b4575b3c0996c1ff465ff633ab44ba205ea9
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-03-10 09:49:15 +0300

    part1 complete

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "channels",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'fontawesomefree',
     'app',
+    'chat',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +78,7 @@ CONTEXT_PROCESSORS = (
 )
 
 WSGI_APPLICATION = 'blog.wsgi.application'
+ASGI_APPLICATION = "blog.asgi.application"
 
 
 # Database
