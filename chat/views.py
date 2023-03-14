d6be4a7b4b8715a2dbb12e97e66e38cdbb705006
Vladimir-82 <grun_gespenst@tut.by>
2023-03-14 20:25:36 +0300

    chat login required

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/login/')
 def index(request):
     return render(request, "chat/index.html")
 
 
+@login_required(login_url='/login/')
 def room(request, room_name):
     user = request.user
     return render(request, "chat/room.html", {"room_name": room_name, "user": user})
