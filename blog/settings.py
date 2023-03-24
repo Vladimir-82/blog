c035249d4fa8e8f7e8b07b4914da5dbf9b7b1ea6
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2023-03-24 09:53:44 +0300

    Captcha math

--- blog/settings.py
+++ blog/settings.py
@@ -173,3 +173,7 @@ EMAIL_HOST_USER = 'grun_gespenst@tut.by'
 EMAIL_HOST_PASSWORD = MAIL
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
+
+
+CAPTCHA_CHALLENGE_FUNCT = 'captcha.helpers.math_challenge'
+CAPTCHA_NOISE_FUNCTIONS = None
